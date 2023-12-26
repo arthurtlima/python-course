2108006ddadfa9a8088daa5d39d129b1868e1fae
Arthur <arthur.alves.lima@live.com.com>
2023-12-26 15:51:18 -0300

    feat: add Python Arithmetic Operators file

---
+++ examples/arithmetic-operators.py
@@ -0,0 +1,45 @@
+# Addition
+x = 5
+y = 3
+print(x + y) # 8
+
+x = y = z = 0
+x = 7
+y = 9
+z = x + y
+print(z) # 16
+
+x = int(input('Digite um número: '))
+y = int(input('Digite outro número: '))
+print(x + y) # resultado de x + y
+
+# Subtraction
+x = 5
+y = 3
+print(x - y) # 2
+
+# Multiplication
+x = 5
+y = 3
+print(x * y) # 15
+
+# Division
+x = 12
+y = 3
+print(x / y) # 4.0
+
+# Modulus
+x = 5
+y = 2
+print(x % y) # 1
+
+# Exponentiation
+x = 2
+y = 5
+print(x ** y) # O mesmo que 2*2*2*2*2 = 32
+
+x = 15
+y = 2
+print(x // y) # 7
+#the floor division // rounds the result down to the nearest whole number
+
