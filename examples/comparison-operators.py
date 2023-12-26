6f14aa1e4bf568c47036104ed0681b399f3e6cb4
Arthur <arthur.alves.lima@live.com.com>
2023-12-26 18:33:23 -0300

    feat: add Python Comparison Operators file

---
+++ examples/comparison-operators.py
@@ -0,0 +1,16 @@
+
+# Comparison Operators
+x = y = z = False
+n1 = n2 = 0
+
+print("Digite um número: ")
+n1 = int(input())
+print("Digite outro número: ")
+n2 = int(input())
+
+print("n1 == n2: ", n1 == n2)
+print("n1 != n2: ", n1 != n2)
+print("n1 > n2: ", n1 > n2)
+print("n1 < n2: ", n1 < n2)
+print("n1 >= n2: ", n1 >= n2)
+print("n1 <= n2: ", n1 <= n2)
